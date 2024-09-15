8733f7fe29267be3279e183e7c678e4a6cb6ae3e
CxrlosKenobi <cxrlos@kenobi.dev>
2024-09-14 22:01:57 -0400

    feat(client): :zap: enhanced codebase and design schema for frontend overall

--- clerk-react/src/routes/dashboard.tsx
+++ clerk-react/src/routes/dashboard.tsx
@@ -1,16 +1,9 @@
-import { Link } from 'react-router-dom'
-
+//
 export default function DashboardPage() {
   return (
-    <>
-      <h1>Dashboard</h1>
-      <p>Your Plan</p>
-
-      <ul>
-        <li>
-          <Link to="/">Return to Home</Link>
-        </li>
-      </ul>
-    </>
+    <section className="custom-wrapper">
+      <h1>This is home</h1>
+      <p>dashboard page as well</p>
+    </section>
   )
 }
--- clerk-react/src/routes/index.tsx
+++ clerk-react/src/routes/index.tsx
@@ -3,24 +3,9 @@
 export default function IndexPage() {
   return (
     <div className="index-container">
-      <h1 className="index-title">Welcome to LinkUp</h1>
+      <h1 className="index-title">Welcome to LinkApp</h1>
       <nav className="index-nav">
         <ul className="index-navbar">
-          {/* <li>
-            <Link to="/sign-up">Sign Up</Link>
-          </li>
-          <li>
-            <Link to="/sign-in">Sign In</Link>
-          </li>
-          <li>
-            <Link to="/app">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/app/uploader">Upload LinkedIn</Link>
-          </li>
-          <li>
-            <Link to="/app/profile">My Profile</Link>
-          </li> */}
         </ul>
       </nav>
     </div>
